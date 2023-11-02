3d12d59974e4738a735b1bf20e6616e71bca9dea
Sajaddoyin <sajaddoyin@gmail.com>
2023-11-02 12:14:53 +0100

    update page

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("hello world")
+
+const welcome = document.querySelector ('.welcome');
+
+const header = document.createElement ('h2');
+
+const headercontent =CreateTextNode('Tech Home.',);
+header .appendChild(headercontent);
+
+welcome .appendChild(header);
